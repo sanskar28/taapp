5f91af84e2c7b58d8f80b5afac7b1a92dcbe0ed3
Sanskar Patni <sanskarpatni28@gmail.com>
2020-06-27 14:57:46 +0530

    1

---
+++ precache-manifest.c3dd1939c411e2dfb6237cea1465c375.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811a930d7705cc654cf768437ed69f49",
+    "url": "/ktjass3react/index.html"
+  },
+  {
+    "revision": "8158c5852eeb7642ef1c",
+    "url": "/ktjass3react/static/css/main.0d7fc3f2.chunk.css"
+  },
+  {
+    "revision": "a609cd1bb4a965718ceb",
+    "url": "/ktjass3react/static/js/2.89806b24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ktjass3react/static/js/2.89806b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8158c5852eeb7642ef1c",
+    "url": "/ktjass3react/static/js/main.f8b2ba7b.chunk.js"
+  },
+  {
+    "revision": "c4f959c1b1d577639262",
+    "url": "/ktjass3react/static/js/runtime-main.0352cd03.js"
+  },
+  {
+    "revision": "c4edc25924b0caf45acf48cc4125cfe9",
+    "url": "/ktjass3react/static/media/Bubai_Manna_(IIT_Kharagpur_main_building)-min (1)-1579328628.c4edc259.jpg"
+  },
+  {
+    "revision": "eb9902991926a76549c629791e14415b",
+    "url": "/ktjass3react/static/media/logo.eb990299.png"
+  }
+]);
